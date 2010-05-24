a65f751bf561ffd5f6f40433d553ef95a69133b6
Anton Narusberg <anton.narusberg@gmail.com>
2010-05-24 14:33:02 +0300

    credential fields in login layout, use login layout for Login Activity

--- gen/com/apprise/toggl/R.java
+++ gen/com/apprise/toggl/R.java
@@ -13,6 +13,11 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int ok=0x7f050002;
+        public static final int password=0x7f050001;
+        public static final int username=0x7f050000;
+    }
     public static final class layout {
         public static final int login=0x7f030000;
         public static final int main=0x7f030001;
