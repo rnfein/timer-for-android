447333aaf4993a7afc1752bdd336f9ece3b209a2
Anton Narusberg <anton.narusberg@gmail.com>
2010-05-24 15:37:36 +0300

    http methods for api communication

--- src/com/apprise/toggl/remote/TogglWebApi.java
+++ src/com/apprise/toggl/remote/TogglWebApi.java
@@ -20,83 +20,85 @@ import org.apache.http.params.HttpParams;
 import android.util.Log;
 
 public class TogglWebApi {
-	
-  private DefaultHttpClient httpClient;	
 
-  private static final int CONNECTION_TIMEOUT = 30 * 1000; // ms
-  
-	private static final String TAG = "TogglWebApi";  
+	private DefaultHttpClient httpClient;
+
+	private static final int CONNECTION_TIMEOUT = 30 * 1000; // ms
+
+	private static final String TAG = "TogglWebApi";
 	private static final String BASE_URL = "http://www.toggl.com";
-  private static final String API_URL = BASE_URL + "/api/v1/";
-  private static final String SESSIONS_URL = API_URL + "/sessions.json";
-  private static final String EMAIL = "email";
-  private static final String PASSWORD = "password";
+	private static final String API_URL = BASE_URL + "/api/v1/";
+	private static final String SESSIONS_URL = API_URL + "/sessions.json";
+	private static final String EMAIL = "email";
+	private static final String PASSWORD = "password";
 
-  public TogglWebApi() {
-  	init();
-  }
-  
-  protected void init() {
-    createHttpClient();    
-  }  
-  
-  public void AuthenticateWithCredentials(String email, String password) {
-    ArrayList<NameValuePair> params = new ArrayList<NameValuePair>();
-    params.add(new BasicNameValuePair(EMAIL, email));
-    params.add(new BasicNameValuePair(PASSWORD, password));
-    
-    HttpResponse response = executePostRequest(SESSIONS_URL, params);
-    
-    Log.d(TAG, "**sessions response:" + response);
-  }
-  
-  public void AuthenticateWithToken(String api_token) {
-  	// TODO
-  }
-  
-	protected HttpResponse executeGetRequest(String url, ArrayList<NameValuePair> params) {
-    StringBuilder uriBuilder = new StringBuilder(url);
+	public TogglWebApi() {
+		init();
+	}
 
-    for(NameValuePair param : params) {
-      uriBuilder.append(params.indexOf(param) == 0 ? "?" : "&");
-      uriBuilder.append(param.getName() + "=" + param.getValue());
-    }      
-    HttpGet request = new HttpGet(uriBuilder.toString());
-    return execute(request);
-  }
+	protected void init() {
+		createHttpClient();
+	}
 
-  protected HttpResponse executePostRequest(String url, ArrayList<NameValuePair> params) {
-    HttpEntity entity = initEntity(params);
-    HttpPost request = new HttpPost(url);
-    request.addHeader(entity.getContentType());
-    request.setEntity(entity);
-    return execute(request);
-  }
+	public void AuthenticateWithCredentials(String email, String password) {
+		ArrayList<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair(EMAIL, email));
+		params.add(new BasicNameValuePair(PASSWORD, password));
 
-  protected HttpEntity initEntity(ArrayList<NameValuePair> params) {
-    try {
-      return new UrlEncodedFormEntity(params, "UTF-8");
-    } catch (final UnsupportedEncodingException e) {
-      // this should never happen.
-      throw new AssertionError(e);
-    }
-  }
-  
-  protected HttpResponse execute(HttpRequestBase request) {
-    try {
-      return httpClient.execute(request);
-    } catch(final IOException e) {
-      Log.d(TAG, "IOException when performing remote request", e);
-      return null;
-    }
-  }	
-  
-  protected void createHttpClient() {
-    httpClient = new DefaultHttpClient();
-    final HttpParams params = httpClient.getParams();
-    
-    HttpConnectionParams.setConnectionTimeout(params, CONNECTION_TIMEOUT);
-    HttpConnectionParams.setSoTimeout(params, CONNECTION_TIMEOUT);
-    ConnManagerParams.setTimeout(params, CONNECTION_TIMEOUT);
-  }  
+		HttpResponse response = executePostRequest(SESSIONS_URL, params);
+
+		Log.d(TAG, "**sessions response:" + response);
+	}
+
+	public void AuthenticateWithToken(String api_token) {
+		// TODO
+	}
+
+	protected HttpResponse executeGetRequest(String url,
+	    ArrayList<NameValuePair> params) {
+		StringBuilder uriBuilder = new StringBuilder(url);
+
+		for (NameValuePair param : params) {
+			uriBuilder.append(params.indexOf(param) == 0 ? "?" : "&");
+			uriBuilder.append(param.getName() + "=" + param.getValue());
+		}
+		HttpGet request = new HttpGet(uriBuilder.toString());
+		return execute(request);
+	}
+
+	protected HttpResponse executePostRequest(String url,
+	    ArrayList<NameValuePair> params) {
+		HttpEntity entity = initEntity(params);
+		HttpPost request = new HttpPost(url);
+		request.addHeader(entity.getContentType());
+		request.setEntity(entity);
+		return execute(request);
+	}
+
+	protected HttpEntity initEntity(ArrayList<NameValuePair> params) {
+		try {
+			return new UrlEncodedFormEntity(params, "UTF-8");
+		} catch (final UnsupportedEncodingException e) {
+			// this should never happen.
+			throw new AssertionError(e);
+		}
+	}
+
+	protected HttpResponse execute(HttpRequestBase request) {
+		try {
+			return httpClient.execute(request);
+		} catch (final IOException e) {
+			Log.d(TAG, "IOException when performing remote request", e);
+			return null;
+		}
+	}
+
+	protected void createHttpClient() {
+		httpClient = new DefaultHttpClient();
+		final HttpParams params = httpClient.getParams();
+
+		HttpConnectionParams.setConnectionTimeout(params, CONNECTION_TIMEOUT);
+		HttpConnectionParams.setSoTimeout(params, CONNECTION_TIMEOUT);
+		ConnManagerParams.setTimeout(params, CONNECTION_TIMEOUT);
+	}
 }
