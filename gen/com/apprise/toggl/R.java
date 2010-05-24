c7fc925d62607d4c33fd008964e39a55b517269b
Anton Narusberg <anton.narusberg@gmail.com>
2010-05-24 16:02:16 +0300

    make auth request on login

--- gen/com/apprise/toggl/R.java
+++ gen/com/apprise/toggl/R.java
@@ -14,9 +14,9 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int ok=0x7f050002;
+        public static final int email=0x7f050000;
+        public static final int login=0x7f050002;
         public static final int password=0x7f050001;
-        public static final int username=0x7f050000;
     }
     public static final class layout {
         public static final int login=0x7f030000;
--- src/com/apprise/toggl/Login.java
+++ src/com/apprise/toggl/Login.java
@@ -1,13 +1,45 @@
 package com.apprise.toggl;
 
+import com.apprise.toggl.remote.TogglWebApi;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends Activity {
+	
+	private TogglWebApi webApi;
+	
+	private EditText emailEditText;
+	private EditText passwordEditText;
+	private Button loginButton; 
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login);
+		webApi = new TogglWebApi();
+		initViews();
+		attachEvents();
+	}
+	
+	protected void initViews() {
+		emailEditText = (EditText) findViewById(R.id.email);
+		passwordEditText = (EditText) findViewById(R.id.password);
+		loginButton = (Button) findViewById(R.id.login);
+	}
+	
+	protected void attachEvents() {
+		loginButton.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				String email = emailEditText.getText().toString();
+				String password = passwordEditText.getText().toString();
+				webApi.AuthenticateWithCredentials(email, password);
+			}
+		});
 	}
 }
