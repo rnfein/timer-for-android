cfc0b8d02fa40a99903ce666bd522e803982cf55
Anton Narusberg <anton.narusberg@gmail.com>
2010-05-24 17:36:49 +0300

    use SharedPreferences to store api token

--- src/com/apprise/toggl/Toggl.java
+++ src/com/apprise/toggl/Toggl.java
@@ -1,31 +1,50 @@
 package com.apprise.toggl;
 
 import android.app.Application;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 
 public class Toggl extends Application {
 
+	public static final String PREF_API_TOKEN = "PREF_API_TOKEN";
+	public static final String TOGGL_PREFS = "TOGGL_PREFS";
+	
+	private Toggl singleton;
+  private SharedPreferences settings;	
+	
+	public Toggl getInstance() {
+		return singleton;
+	}
+	
+	public void storeAPIToken(String apiToken) {
+		SharedPreferences.Editor editor = settings.edit();
+		editor.putString(PREF_API_TOKEN, apiToken);
+		editor.commit();
+	}
+	
+	public String getAPIToken() {
+    return settings.getString(PREF_API_TOKEN, null);		
+	}
+	
 	@Override
   public void onConfigurationChanged(Configuration newConfig) {
-	  // TODO Auto-generated method stub
 	  super.onConfigurationChanged(newConfig);
   }
 
 	@Override
   public void onCreate() {
-	  // TODO Auto-generated method stub
 	  super.onCreate();
+    singleton = this;
+    settings = getSharedPreferences(TOGGL_PREFS, MODE_PRIVATE);	  
   }
 
 	@Override
   public void onLowMemory() {
-	  // TODO Auto-generated method stub
 	  super.onLowMemory();
   }
 
 	@Override
   public void onTerminate() {
-	  // TODO Auto-generated method stub
 	  super.onTerminate();
   }
 
