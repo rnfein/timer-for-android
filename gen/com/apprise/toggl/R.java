bff3888e26cc7d0dff06a719e1eef285ed9873ed
Anton Narusberg <anton.narusberg@gmail.com>
2010-05-24 21:05:27 +0300

    Open browser on signup page when 'Create new account' is clicked

--- gen/com/apprise/toggl/R.java
+++ gen/com/apprise/toggl/R.java
@@ -10,19 +10,23 @@ package com.apprise.toggl;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int blue_text=0x7f040000;
+    }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int email=0x7f050000;
-        public static final int login=0x7f050002;
-        public static final int password=0x7f050001;
+        public static final int create_account=0x7f060000;
+        public static final int email=0x7f060001;
+        public static final int login=0x7f060003;
+        public static final int password=0x7f060002;
     }
     public static final class layout {
         public static final int login=0x7f030000;
         public static final int main=0x7f030001;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
+        public static final int app_name=0x7f050000;
     }
 }
--- src/com/apprise/toggl/Login.java
+++ src/com/apprise/toggl/Login.java
@@ -5,11 +5,15 @@ import com.apprise.toggl.storage.User;
 import com.google.gson.Gson;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends Activity {
 	
@@ -17,10 +21,12 @@ public class Login extends Activity {
 	
 	private EditText emailEditText;
 	private EditText passwordEditText;
+	private TextView createNewAccount;
 	private Button loginButton;
 	private Toggl app;
 	
 	private static final String TAG = "Login";
+	private static final String CREATE_NEW_ACCOUNT_URL = "https://www.toggl.com/signup";
 	
 	/** Called when the activity is first created. */
 	@Override
@@ -38,6 +44,7 @@ public class Login extends Activity {
 		emailEditText = (EditText) findViewById(R.id.email);
 		passwordEditText = (EditText) findViewById(R.id.password);
 		loginButton = (Button) findViewById(R.id.login);
+		createNewAccount = (TextView) findViewById(R.id.create_account);
 	}
 	
 	protected void attachEvents() {
@@ -53,6 +60,21 @@ public class Login extends Activity {
 				Log.d(TAG, "user:" + user);
 				
 				app.storeAPIToken(user.apiToken);
+			}
+		});
+		
+		createNewAccount.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				Intent i = new Intent();
+				ComponentName comp =
+				new ComponentName("com.android.browser", "com.android.browser.BrowserActivity");
+				i.setComponent(comp);
+				i.setAction("android.intent.action.VIEW");
+				i.addCategory("android.intent.category.BROWSABLE");
+				Uri uri = Uri.parse(CREATE_NEW_ACCOUNT_URL);
+				i.setData(uri);
+				startActivity(i);				
 			}
 		});
 	}
