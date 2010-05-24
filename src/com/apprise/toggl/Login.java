b0af4e938cb0958b0b2f4acd9edc809b580fb67a
Anton Narusberg <anton.narusberg@gmail.com>
2010-05-24 15:32:09 +0300

    indentation

--- src/com/apprise/toggl/Login.java
+++ src/com/apprise/toggl/Login.java
@@ -4,10 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class Login extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login);
+	}
 }
