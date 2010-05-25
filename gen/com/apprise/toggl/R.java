f80815a716d2024a102600b04462cbf73d9a3a22
Anton Narusberg <anton.narusberg@gmail.com>
2010-05-25 09:33:08 +0300

    Tasks Activity skeleton

--- gen/com/apprise/toggl/R.java
+++ gen/com/apprise/toggl/R.java
@@ -25,6 +25,7 @@ public final class R {
     public static final class layout {
         public static final int login=0x7f030000;
         public static final int main=0x7f030001;
+        public static final int tasks=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
---
+++ src/com/apprise/toggl/Tasks.java
@@ -0,0 +1,14 @@
+package com.apprise.toggl;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Tasks extends Activity {
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.tasks);
+  }
+  
+}
