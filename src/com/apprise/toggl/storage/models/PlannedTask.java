9e33640677f5c3273a1c39f8670e6b8cefdebb17
Anton Narusberg <anton.narusberg@gmail.com>
2010-05-28 01:07:20 +0300

    Planned task model

---
+++ src/com/apprise/toggl/storage/models/PlannedTask.java
@@ -0,0 +1,10 @@
+package com.apprise.toggl.storage.models;
+
+public class PlannedTask {
+  public String name;
+  public Workspace workspace;
+  public long id;    
+  public Project project;
+  public User user;
+  public int estimated_workhours;
+}
