b4478f4e9f009ce3b264cf645cf9817b186bf1a9
Anton Narusberg <anton.narusberg@gmail.com>
2010-05-29 16:27:36 +0300

    dbadapter refactor: fields ordering conventions, follow dependencies

--- src/com/apprise/toggl/storage/DatabaseAdapter.java
+++ src/com/apprise/toggl/storage/DatabaseAdapter.java
@@ -170,6 +170,7 @@ public class DatabaseAdapter {
 
     private static final String CREATE_USERS_TABLE = "CREATE TABLE " + Users.TABLE_NAME + " ("
       + Users._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+      + Users.REMOTE_ID + " INTEGER NOT NULL,"      
       + Users.JQUERY_TIMEOFDAY_FORMAT + " TEXT,"
       + Users.API_TOKEN + " TEXT,"
       + Users.TASK_RETENTION_DAYS + " INTEGER,"
@@ -179,58 +180,56 @@ public class DatabaseAdapter {
       + Users.NEW_TASKS_START_AUTOMATICALLY + " TEXT,"
       + Users.FULLNAME + " TEXT,"
       + Users.LANGUAGE + " TEXT,"
-      + Users.REMOTE_ID + " INTEGER NOT NULL,"
       + Users.BEGINNING_OF_WEEK + " INTEGER,"
       + Users.TIMEODAY_FORMAT + " TEXT,"
       + Users.EMAIL + " TEXT"
       + ");";
+
+    private static final String CREATE_WORKSPACES_TABLE = "CREATE TABLE " + Workspaces.TABLE_NAME + " ("
+      + Workspaces._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+      + Workspaces.REMOTE_ID + " INTEGER NOT NULL,"      
+      + Workspaces.NAME + " TEXT"
+      + ");";
     
     private static final String CREATE_PROJECTS_TABLE = "CREATE TABLE " + Projects.TABLE_NAME + " ("
       + Projects._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+      + Projects.REMOTE_ID + " INTEGER NOT NULL,"
+      + Projects.SYNC_DIRTY + " INTEGER NOT NULL,"      
       + Projects.FIXED_FEE + " INTEGER,"
       + Projects.ESTIMATED_WORKHOURS + " INTEGER,"
       + Projects.IS_FIXED_FEE + " INTEGER NOT NULL,"
-      + Projects.WORKSPACE_REMOTE_ID + " INTEGER NOT NULL,"
+      + Projects.WORKSPACE_ID + " INTEGER NOT NULL,"
       + Projects.BILLABLE + " INTEGER NOT NULL,"
       + Projects.CLIENT_PROJECT_NAME + " TEXT,"
       + Projects.HOURLY_RATE + " INTEGER NOT NULL,"
-      + Projects.NAME + " TEXT,"
-      + Projects.REMOTE_ID + " INTEGER NOT NULL,"
-      + Projects.SYNC_DIRTY + " INTEGER NOT NULL"
+      + Projects.NAME + " TEXT"
       + ");";
-
     
     private static final String CREATE_TASKS_TABLE = "CREATE TABLE " + Tasks.TABLE_NAME + " ("
       + Tasks._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
-      + Tasks.PROJECT_REMOTE_ID + " INTEGER NOT NULL,"
-      + Tasks.WORKSPACE_REMOTE_ID + " INTEGER NOT NULL,"
+      + Tasks.REMOTE_ID + " INTEGER NOT NULL,"      
+      + Tasks.PROJECT_ID + " INTEGER NOT NULL,"
+      + Tasks.WORKSPACE_ID + " INTEGER NOT NULL,"
       + Tasks.DURATION + " INTEGER NOT NULL,"
       + Tasks.START + " TEXT,"
       + Tasks.BILLABLE + " INTEGER NOT NULL,"
       + Tasks.DESCRIPTION + " TEXT,"
       + Tasks.STOP + " TEXT,"
-      + Tasks.TAG_NAMES + " TEXT,"
-      + Tasks.REMOTE_ID + " INTEGER NOT NULL"
+      + Tasks.TAG_NAMES + " TEXT"
       + ");";
     
     private static final String CREATE_DELETED_TASKS_TABLE = "CREATE TABLE " + DeletedTasks.TABLE_NAME + " ("
     + DeletedTasks._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
     + DeletedTasks.TASK_ID + " INTEGER NOT NULL"
     + ");";
-
-    private static final String CREATE_WORKSPACES_TABLE = "CREATE TABLE " + Workspaces.TABLE_NAME + " ("
-      + Workspaces._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
-      + Workspaces.NAME + " TEXT,"
-      + Workspaces.REMOTE_ID + " INTEGER NOT NULL"
-      + ");";
     
     private static final String CREATE_PLANNED_TASKS_TABLE = "CREATE TABLE " + PlannedTasks.TABLE_NAME + " ("
       + PlannedTasks._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+      + PlannedTasks.REMOTE_ID + " INTEGER NOT NULL,"      
       + PlannedTasks.NAME + " TEXT,"
-      + PlannedTasks.WORKSPACE_REMOTE_ID + " INTEGER NOT NULL,"
-      + PlannedTasks.REMOTE_ID + " INTEGER NOT NULL,"
-      + PlannedTasks.PROJECT_REMOTE_ID + " INTEGER NOT NULL,"
-      + PlannedTasks.USER_REMOTE_ID + " INTEGER NOT NULL,"
+      + PlannedTasks.WORKSPACE_ID + " INTEGER NOT NULL,"
+      + PlannedTasks.PROJECT_ID + " INTEGER NOT NULL,"
+      + PlannedTasks.USER_ID + " INTEGER NOT NULL,"
       + PlannedTasks.ESTIMATED_WORKHOURS + " INTEGER NOT NULL"
     + ");";
 
@@ -238,8 +237,8 @@ public class DatabaseAdapter {
     @Override
     public void onCreate(SQLiteDatabase db) {
       db.execSQL(CREATE_USERS_TABLE);
+      db.execSQL(CREATE_WORKSPACES_TABLE);      
       db.execSQL(CREATE_PROJECTS_TABLE);
-      db.execSQL(CREATE_WORKSPACES_TABLE);
       db.execSQL(CREATE_TASKS_TABLE);
       db.execSQL(CREATE_DELETED_TASKS_TABLE);
       db.execSQL(CREATE_PLANNED_TASKS_TABLE);
@@ -254,6 +253,7 @@ public class DatabaseAdapter {
   public static final class Users implements BaseColumns {
     public static final String TABLE_NAME = "users";
     
+    public static final String REMOTE_ID = "remote_id";    
     public static final String JQUERY_TIMEOFDAY_FORMAT = "jquery_timeofday_format";
     public static final String API_TOKEN = "api_token";
     public static final String TASK_RETENTION_DAYS = "task_retention_days";
@@ -263,48 +263,47 @@ public class DatabaseAdapter {
     public static final String NEW_TASKS_START_AUTOMATICALLY = "new_tasks_start_automatically";
     public static final String FULLNAME = "fullname";
     public static final String LANGUAGE = "language";
-    public static final String REMOTE_ID = "remote_id";
     public static final String BEGINNING_OF_WEEK = "beginning_of_week";
     public static final String TIMEODAY_FORMAT = "timeofday_format";
     public static final String EMAIL = "email";
     
   }  
   
-  public static final class Projects implements BaseColumns {
-    public static final String TABLE_NAME = "projects";
-    
-    public static final String FIXED_FEE = "fixed_fee";
-    public static final String ESTIMATED_WORKHOURS = "estimated_workhours";
-    public static final String IS_FIXED_FEE = "is_fixed_fee";
-    public static final String WORKSPACE_REMOTE_ID = "workspace_remote_id";
-    public static final String BILLABLE = "billable";
-    public static final String CLIENT_PROJECT_NAME = "client_project_name";
-    public static final String HOURLY_RATE = "hourly_rate";
-    public static final String NAME = "name";
-    public static final String REMOTE_ID = "remote_id";
-    public static final String SYNC_DIRTY = "sync_dirty";    
-  }
-  
   public static final class Workspaces implements BaseColumns {
     public static final String TABLE_NAME = "workspaces";
     
+    public static final String REMOTE_ID = "remote_id";    
     public static final String NAME = "name";
+  }
+  
+  public static final class Projects implements BaseColumns {
+    public static final String TABLE_NAME = "projects";
+    
     public static final String REMOTE_ID = "remote_id";
+    public static final String SYNC_DIRTY = "sync_dirty";     
+    public static final String FIXED_FEE = "fixed_fee";
+    public static final String ESTIMATED_WORKHOURS = "estimated_workhours";
+    public static final String IS_FIXED_FEE = "is_fixed_fee";
+    public static final String WORKSPACE_ID = "workspace_remote_id";
+    public static final String BILLABLE = "billable";
+    public static final String CLIENT_PROJECT_NAME = "client_project_name";
+    public static final String HOURLY_RATE = "hourly_rate";
+    public static final String NAME = "name";   
   }
   
   public static final class Tasks implements BaseColumns {
     public static final String TABLE_NAME = "tasks";
 
-    public static final String PROJECT_REMOTE_ID = "project_remote_id";
-    public static final String WORKSPACE_REMOTE_ID = "workspace_remote_id";
+    public static final String REMOTE_ID = "remote_id";
+    public static final String SYNC_DIRTY = "sync_dirty";     
+    public static final String PROJECT_ID = "project_id";
+    public static final String WORKSPACE_ID = "workspace_id";
     public static final String DURATION = "duration";
     public static final String START = "start";
     public static final String BILLABLE = "billable";
     public static final String DESCRIPTION = "description";
     public static final String STOP = "stop";
     public static final String TAG_NAMES = "tag_names";
-    public static final String REMOTE_ID = "remote_id";
-    public static final String SYNC_DIRTY = "sync_dirty";    
   }
   
   public static final class DeletedTasks implements BaseColumns {
@@ -316,11 +315,11 @@ public class DatabaseAdapter {
   public static final class PlannedTasks implements BaseColumns {
     public static final String TABLE_NAME = "planned_tasks";
     
+    public static final String REMOTE_ID = "remote_id";      
     public static final String NAME = "name";
-    public static final String WORKSPACE_REMOTE_ID = "workspace_remote_id";
-    public static final String REMOTE_ID = "remote_id";    
-    public static final String PROJECT_REMOTE_ID = "project_remote_id";
-    public static final String USER_REMOTE_ID = "user_remote_id";
+    public static final String WORKSPACE_ID = "workspace_id";  
+    public static final String PROJECT_ID = "project_id";
+    public static final String USER_ID = "user_id";
     public static final String ESTIMATED_WORKHOURS = "estimated_workhours";
   }
   
