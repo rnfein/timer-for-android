556fd72202d13ff0e94e54f8ebe97f0a22d7ab7b
Kevin Valdek <kevinvaldek@gmail.com>
2010-05-31 17:23:45 +0300

    provide menu option to get back from signup.

--- src/com/apprise/toggl/SignUpActivity.java
+++ src/com/apprise/toggl/SignUpActivity.java
@@ -1,6 +1,8 @@
 package com.apprise.toggl;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.Window;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
@@ -8,6 +10,8 @@ import android.webkit.WebView;
 public class SignUpActivity extends ApplicationActivity {
   
   private static final String SIGNUP_URL = "https://www.toggl.com/signup";
+  private static final int DEFAULT_CATEGORY = 0;
+  private static final int BACK_TO_ACCOUNT_OPTION = Menu.FIRST;
 
   private WebView webView;
   
@@ -20,6 +24,23 @@ public class SignUpActivity extends ApplicationActivity {
     
     initWebClient();
   }
+  
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    menu.add(DEFAULT_CATEGORY, BACK_TO_ACCOUNT_OPTION, Menu.NONE, R.string.account).setIcon(android.R.drawable.ic_menu_preferences);
+    return super.onCreateOptionsMenu(menu);
+  }
+
+  @Override
+  public boolean onMenuItemSelected(int featureId, MenuItem item) {
+    switch (item.getItemId()) {
+      case BACK_TO_ACCOUNT_OPTION:
+        finish();
+        return true;
+    }
+
+    return super.onMenuItemSelected(featureId, item);
+  } 
   
   private void initWebClient() {
     webView.getSettings().setJavaScriptEnabled(true);
