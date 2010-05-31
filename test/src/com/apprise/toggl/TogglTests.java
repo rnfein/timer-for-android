6ae6d0ff8773325476957fa1ac6b9c7b44a2b2b5
Kevin Valdek <kevinvaldek@gmail.com>
2010-05-31 11:13:34 +0300

    renamed AllTests class to TogglTests.

---
+++ test/src/com/apprise/toggl/TogglTests.java
@@ -0,0 +1,35 @@
+package com.apprise.toggl;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import android.test.suitebuilder.TestSuiteBuilder;
+
+/**
+ * A test suite containing all tests for runner.
+ *
+ * To run all suites found in this apk:
+ * $ adb shell am instrument -w com.apprise.toggl.tests/android.test.InstrumentationTestRunner
+ *  
+ *
+ * To run just this suite from the command line:
+ * $ adb shell am instrument -w \
+ *   -e class com.apprise.toggl.TogglTests \
+ *   com.apprise.toggl.tests/android.test.InstrumentationTestRunner
+ *
+ * To run an individual test case, e.g. {@link com.example.android.apis.os.MorseCodeConverterTest}:
+ * $ adb shell am instrument -w \
+ *   -e class com.example.android.apis.os.MorseCodeConverterTest \
+ *   com.example.android.apis.tests/android.test.InstrumentationTestRunner
+ *
+ * To run an individual test, e.g. {@link com.example.android.apis.os.MorseCodeConverterTest#testCharacterS()}:
+ * $ adb shell am instrument -w \
+ *   -e class com.example.android.apis.os.MorseCodeConverterTest#testCharacterS \
+ *   com.example.android.apis.tests/android.test.InstrumentationTestRunner
+ */
+public class TogglTests extends TestSuite {
+
+  public static Test suite() {
+    return new TestSuiteBuilder(TogglTests.class).includeAllPackagesUnderHere().build();
+  }
+}
