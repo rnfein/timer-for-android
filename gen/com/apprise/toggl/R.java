88e3cc58df527fe54ca7639e83e2bf07c0aa3629
Anton Narusberg <anton.narusberg@gmail.com>
2010-06-01 16:41:33 +0300

    Tasks layout, design implementation tweaks

--- gen/com/apprise/toggl/R.java
+++ gen/com/apprise/toggl/R.java
@@ -12,26 +12,34 @@ public final class R {
     }
     public static final class color {
         public static final int black=0x7f040001;
-        public static final int heading_red=0x7f040003;
-        public static final int light_blue=0x7f040002;
+        public static final int heading_red=0x7f040004;
+        public static final int light_blue=0x7f040003;
+        public static final int light_gray=0x7f040002;
         public static final int transparent=0x7f040000;
     }
     public static final class dimen {
-        public static final int label_padding_left=0x7f050002;
-        public static final int label_padding_top=0x7f050001;
-        public static final int label_text_size=0x7f050000;
-        public static final int login_edittext_padding_right=0x7f050004;
-        public static final int login_edittext_size=0x7f050003;
+        public static final int label_padding_left=0x7f050005;
+        public static final int label_text_size=0x7f050004;
+        public static final int layout_padding_bottom=0x7f050002;
+        public static final int layout_padding_left=0x7f050003;
+        public static final int layout_padding_right=0x7f050001;
+        public static final int layout_padding_top=0x7f050000;
+        public static final int listitem_padding_left=0x7f050007;
+        public static final int listitem_text_size=0x7f050006;
+        public static final int login_edittext_padding_right=0x7f05000a;
+        public static final int login_edittext_size=0x7f050009;
+        public static final int task_time_text_size=0x7f050008;
     }
     public static final class drawable {
         public static final int arrow=0x7f020000;
         public static final int bg=0x7f020001;
-        public static final int icon=0x7f020002;
-        public static final int textfield=0x7f020003;
-        public static final int trigger=0x7f020004;
-        public static final int txt_bottom=0x7f020005;
-        public static final int txt_middle=0x7f020006;
-        public static final int txt_top=0x7f020007;
+        public static final int divider=0x7f020002;
+        public static final int icon=0x7f020003;
+        public static final int textfield=0x7f020004;
+        public static final int trigger=0x7f020005;
+        public static final int txt_bottom=0x7f020006;
+        public static final int txt_middle=0x7f020007;
+        public static final int txt_top=0x7f020008;
     }
     public static final class id {
         public static final int account_menu_log_out=0x7f080006;
@@ -60,12 +68,13 @@ public final class R {
     public static final class string {
         public static final int account=0x7f060002;
         public static final int app_name=0x7f060000;
-        public static final int authentication_failed=0x7f060004;
-        public static final int create_account=0x7f060008;
-        public static final int email=0x7f060005;
-        public static final int log_in=0x7f060007;
-        public static final int log_out=0x7f060003;
-        public static final int password=0x7f060006;
+        public static final int authentication_failed=0x7f060005;
+        public static final int create_account=0x7f060009;
+        public static final int email=0x7f060006;
+        public static final int log_in=0x7f060008;
+        public static final int log_out=0x7f060004;
+        public static final int password=0x7f060007;
         public static final int refresh=0x7f060001;
+        public static final int tasks=0x7f060003;
     }
 }
