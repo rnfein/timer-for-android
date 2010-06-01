8f0cc2bab056529f90de8965bbde1f0ee57b2737
Kevin Valdek <kevinvaldek@gmail.com>
2010-06-01 17:21:51 +0300

    added failing test for account activity.

---
+++ test/src/com/apprise/toggl/AccountActivityTests.java
@@ -0,0 +1,40 @@
+package com.apprise.toggl;
+
+import android.app.Activity;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
+
+public class AccountActivityTests extends ActivityInstrumentationTestCase2<AccountActivity>{
+
+  public AccountActivityTests() {
+    super("com.apprise.toggl", AccountActivity.class);
+  }
+  
+  public void testStoreEditTextsOnKill() {
+    Activity activity = getActivity();
+    
+    final Activity uiRef = activity;
+    activity.runOnUiThread(new Runnable() {
+      
+      public void run() {
+        EditText email = (EditText) uiRef.findViewById(R.id.email);
+        EditText password = (EditText) uiRef.findViewById(R.id.password);
+
+        email.setText("ted@toggl.com");
+        password.setText("tog..");                
+      }
+    });
+    
+    // kill activity
+    activity.finish();
+    
+    // revive activity
+    activity = getActivity();
+    EditText email = (EditText) activity.findViewById(R.id.email);
+    EditText password = (EditText) activity.findViewById(R.id.password);
+
+    assertEquals("ted@toggl.com", email.getText().toString());
+    assertEquals("tog..", password.getText().toString());
+  }
+  
+}
