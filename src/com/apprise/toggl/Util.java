37b7963241bcf973ca1bfb33d60424e33b54be94
Anton Narusberg <anton.narusberg@gmail.com>
2010-06-02 16:58:01 +0300

    util methods to parse date to iso8601 string and otherway around

--- src/com/apprise/toggl/Util.java
+++ src/com/apprise/toggl/Util.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Util {
-
+  
   public static String inputStreamToString(InputStream is) {
     StringBuilder out = new StringBuilder();
     try {
@@ -23,6 +25,24 @@ public class Util {
       // ignore
     }
     return out.length() > 0 ? out.toString() : null;
+  }
+  
+  public static Date parseStringToDate(String dateString) {
+    SimpleDateFormat iso8601Format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    Date date = null;
+    try {
+      date = iso8601Format.parse(dateString);
+    } catch (java.text.ParseException e) {
+      e.printStackTrace();
+    }
+    return date;
+  }
+  
+  public static String parseDateToString(Date date) {
+    SimpleDateFormat iso8601Format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    String dateString = null;
+    dateString = iso8601Format.format(date);
+    return dateString;
   }
 
 }
