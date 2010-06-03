bad5374cdf5de3608ef6ff97ae2d08b146d36bdb
Anton Narusberg <anton.narusberg@gmail.com>
2010-06-03 19:42:12 +0300

    Task layout

--- gen/com/apprise/toggl/R.java
+++ gen/com/apprise/toggl/R.java
@@ -19,17 +19,19 @@ public final class R {
         public static final int white=0x7f040002;
     }
     public static final class dimen {
-        public static final int label_padding_left=0x7f050005;
+        public static final int label_padding=0x7f050005;
+        public static final int label_padding_left=0x7f050006;
         public static final int label_text_size=0x7f050004;
         public static final int layout_padding_bottom=0x7f050002;
         public static final int layout_padding_left=0x7f050003;
         public static final int layout_padding_right=0x7f050001;
         public static final int layout_padding_top=0x7f050000;
-        public static final int listitem_note_text_size=0x7f050008;
-        public static final int listitem_padding_left=0x7f050007;
-        public static final int listitem_text_size=0x7f050006;
-        public static final int login_edittext_padding_right=0x7f05000a;
-        public static final int login_edittext_size=0x7f050009;
+        public static final int listitem_note_text_size=0x7f050009;
+        public static final int listitem_padding_left=0x7f050008;
+        public static final int listitem_text_size=0x7f050007;
+        public static final int login_edittext_padding_right=0x7f05000b;
+        public static final int login_edittext_size=0x7f05000a;
+        public static final int task_timer_text_size=0x7f05000c;
     }
     public static final class drawable {
         public static final int arrow=0x7f020000;
@@ -43,18 +45,20 @@ public final class R {
         public static final int txt_top=0x7f020008;
     }
     public static final class id {
-        public static final int account_menu_log_out=0x7f080008;
+        public static final int account_menu_log_out=0x7f08000a;
         public static final int create_account=0x7f080003;
         public static final int email=0x7f080000;
         public static final int login=0x7f080002;
         public static final int password=0x7f080001;
-        public static final int signup_menu_return=0x7f080009;
-        public static final int task_item_client_project_name=0x7f080005;
-        public static final int task_item_description=0x7f080004;
-        public static final int task_item_duration=0x7f080006;
-        public static final int task_list_header_text=0x7f080007;
-        public static final int tasks_menu_account=0x7f08000b;
-        public static final int tasks_menu_refresh=0x7f08000a;
+        public static final int signup_menu_return=0x7f08000b;
+        public static final int task_item_client_project_name=0x7f080007;
+        public static final int task_item_description=0x7f080006;
+        public static final int task_item_duration=0x7f080008;
+        public static final int task_list_header_text=0x7f080009;
+        public static final int task_timer_area=0x7f080004;
+        public static final int task_timer_duration=0x7f080005;
+        public static final int tasks_menu_account=0x7f08000d;
+        public static final int tasks_menu_refresh=0x7f08000c;
     }
     public static final class layout {
         public static final int account=0x7f030000;
@@ -73,13 +77,19 @@ public final class R {
     public static final class string {
         public static final int account=0x7f060002;
         public static final int app_name=0x7f060000;
-        public static final int authentication_failed=0x7f060005;
-        public static final int create_account=0x7f060009;
-        public static final int email=0x7f060006;
-        public static final int log_in=0x7f060008;
-        public static final int log_out=0x7f060004;
-        public static final int password=0x7f060007;
+        public static final int authentication_failed=0x7f06000b;
+        public static final int billable=0x7f060006;
+        public static final int create_account=0x7f06000f;
+        public static final int date=0x7f060005;
+        public static final int email=0x7f06000c;
+        public static final int log_in=0x7f06000e;
+        public static final int log_out=0x7f06000a;
+        public static final int password=0x7f06000d;
+        public static final int planned=0x7f060007;
+        public static final int project=0x7f060009;
         public static final int refresh=0x7f060001;
-        public static final int tasks=0x7f060003;
+        public static final int tags=0x7f060008;
+        public static final int task=0x7f060003;
+        public static final int tasks=0x7f060004;
     }
 }
