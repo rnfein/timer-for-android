6fffc2a471ab8a59774f843ffae532527b485deb
Anton Narusberg <anton.narusberg@gmail.com>
2010-06-06 08:51:58 +0300

    Projects Activity skeleton

---
+++ src/com/apprise/toggl/ProjectsActivity.java
@@ -0,0 +1,14 @@
+package com.apprise.toggl;
+
+import android.os.Bundle;
+
+public class ProjectsActivity extends ApplicationActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    // TODO Auto-generated method stub
+    super.onCreate(savedInstanceState);
+  }
+
+  
+}
