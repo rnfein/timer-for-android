032ead5fe6e7dc68adb90005cc2a53ec2c41216e
Kevin Valdek <kevinvaldek@gmail.com>
2010-06-08 14:56:44 +0300

    added time tracking service skeleton.

---
+++ src/com/apprise/toggl/tracking/TimeTrackingService.java
@@ -0,0 +1,14 @@
+package com.apprise.toggl.tracking;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class TimeTrackingService extends Service {
+
+  @Override
+  public IBinder onBind(Intent arg0) {
+    return null;
+  }
+
+}
---
+++ test/src/com/apprise/toggl/tracking/TimeTrackingServiceTest.java
@@ -0,0 +1,13 @@
+package com.apprise.toggl.tracking;
+
+import com.apprise.toggl.tracking.TimeTrackingService;
+
+import android.test.ServiceTestCase;
+
+public class TimeTrackingServiceTest extends ServiceTestCase<TimeTrackingService> {
+
+  public TimeTrackingServiceTest() {
+    super(TimeTrackingService.class);
+  }
+  
+}
