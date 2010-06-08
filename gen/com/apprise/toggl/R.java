d295429b59ec074b4ff0331868dd98ea5d5a665d
Anton Narusberg <anton.narusberg@gmail.com>
2010-06-08 18:09:41 +0300

    Deprecate ProjectsActivity

--- gen/com/apprise/toggl/R.java
+++ gen/com/apprise/toggl/R.java
@@ -82,12 +82,11 @@ public final class R {
         public static final int main=0x7f030001;
         public static final int project_dropdown_item=0x7f030002;
         public static final int project_item=0x7f030003;
-        public static final int projects=0x7f030004;
-        public static final int task=0x7f030005;
-        public static final int task_item=0x7f030006;
-        public static final int tasks=0x7f030007;
-        public static final int tasks_group_header=0x7f030008;
-        public static final int welcome=0x7f030009;
+        public static final int task=0x7f030004;
+        public static final int task_item=0x7f030005;
+        public static final int tasks=0x7f030006;
+        public static final int tasks_group_header=0x7f030007;
+        public static final int welcome=0x7f030008;
     }
     public static final class menu {
         public static final int account_menu=0x7f070000;
