542f6512aafbdc52ed57607330a1cee0c3f1c74c
Anton Narusberg <anton.narusberg@gmail.com>
2010-06-09 18:49:00 +0300

    hardcoded json build

--- src/com/apprise/toggl/storage/models/Project.java
+++ src/com/apprise/toggl/storage/models/Project.java
@@ -1,5 +1,12 @@
 package com.apprise.toggl.storage.models;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import com.apprise.toggl.Toggl;
+import com.google.gson.Gson;
+
+
 public class Project extends Model {
   
   public float fixed_fee;
@@ -88,6 +95,59 @@ public class Project extends Model {
     client = otherProject.client;
     hourly_rate = otherProject.hourly_rate;
     name = otherProject.name;
+  }
+  
+  public String apiJsonString() {
+    JSONArray rootObj = new JSONArray();
+    JSONArray projectObj = new JSONArray();
+    JSONArray workspaceObj = new JSONArray();
+    JSONArray clientObj = new JSONArray();
+    
+    Toggl app = new Toggl().getInstance();
+    
+    workspaceObj.put("id:");
+    if (this.workspace != null) {      
+      workspaceObj.put(this.workspace.id);      
+    } else {
+      workspaceObj.put(48003);
+    }
+    
+    if (this.client != null) {
+      clientObj.put("id:");
+      clientObj.put(client.id);
+    } 
+    
+    try {
+      projectObj.put("fixed_fee:");
+      projectObj.put(this.fixed_fee);
+      projectObj.put("estimated_workhours:");
+      projectObj.put(this.estimated_workhours);
+      projectObj.put("fixed_fee:");
+      projectObj.put(this.fixed_fee);
+      projectObj.put("is_fixed_fee:");
+      projectObj.put(this.is_fixed_fee);
+      projectObj.put("workspace:");
+      projectObj.put(workspaceObj.toString());
+      projectObj.put("billable:");
+      projectObj.put(this.billable);
+      projectObj.put("client_project_name:");
+      projectObj.put(this.client_project_name);
+      projectObj.put("hourly_rate:");
+      projectObj.put(this.hourly_rate);
+      projectObj.put("client:");
+      if (this.client != null) {
+        projectObj.put(clientObj.toString());
+      }
+      projectObj.put("name:");
+      projectObj.put(this.name);
+    } catch (JSONException e) {
+      e.printStackTrace();
+    }
+    
+    rootObj.put("project:");
+    rootObj.put(projectObj.toString());
+    
+    return rootObj.toString();
   }
   
 }
