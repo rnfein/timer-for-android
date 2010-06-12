4cf3ae64843c195769aba025a9cb6cbb298cdb65
Anton Narusberg <anton.narusberg@gmail.com>
2010-06-12 17:49:10 +0300

    lose the finish Activity if logged out.. instead show empty list

--- src/com/apprise/toggl/ApplicationActivity.java
+++ src/com/apprise/toggl/ApplicationActivity.java
@@ -10,14 +10,6 @@ public class ApplicationActivity extends Activity {
 
   private Toggl app;
 
-  @Override
-  protected void onResume() {
-    app = (Toggl) getApplication();
-    if (app.getCurrentUser() == null)
-      finish();
-    super.onResume();
-  }
-  
   /**
    * Helper method to get the index of the cursor item that matches:
    *   model._id == item._id
